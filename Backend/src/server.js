fafb614e40194c0c189cda925b3302a3c9dcb2b6
asadalijaffari777-eng <asadalijaffari777@gmail.com>
2026-07-12 19:43:28 +0500

    fix: await MongoDB connection before starting server

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -3,7 +3,11 @@ require('dotenv').config({ path: path.join(__dirname, '../.env') });
 const app = require('./app')
 const connectDB = require('./config/database')
 
-connectDB();
-app.listen(3001, ()=>{
+connectDB().then(() => {
+  app.listen(3001, () => {
     console.log('server is running on port 3001')
+  })
+}).catch(err => {
+  console.error('Failed to start server:', err)
+  process.exit(1)
 })
